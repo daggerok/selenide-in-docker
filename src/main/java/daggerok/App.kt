4123ef64b0e96dbabd1c50de2bb691bf9ffe57d0
Maksim Kostromin <daggerok@gmail.com>
2018-05-30 09:43:27 +0300

    Initial commit

---
+++ src/main/java/daggerok/App.kt
@@ -0,0 +1,81 @@
+package daggerok
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.reactive.result.view.Rendering
+import java.util.*
+
+@ControllerAdvice
+class ModelAdvice {
+  @ModelAttribute("pseudoStatefulModel")
+  fun pseudoStatefulModel() = mutableMapOf("message" to "ololo trololo")
+}
+
+fun MutableMap<String, Any>.greet(payload: String): MutableMap<String, Any> {
+  this["message"] = "Hello, ${payload.capitalize()}!"
+  return this
+}
+
+@Controller
+class IndexPage {
+
+  @GetMapping(path = ["", "/"])
+  fun index(@RequestParam(required = false, name = "name") name: Optional<String>,
+            @ModelAttribute("pseudoStatefulModel") pseudoStatefulModel: MutableMap<String, Any>) =
+      Rendering.view("index")
+          .model(pseudoStatefulModel.greet(name.orElse("my friend")))
+          .build()
+
+  @GetMapping("/{name}")
+  fun path(@PathVariable("name") name: String,
+           @ModelAttribute("pseudoStatefulModel") pseudoStatefulModel: MutableMap<String, Any>) =
+      Rendering.view("index")
+          .model(pseudoStatefulModel.greet(name))
+          .build()
+}
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+class SecurityConfig {
+
+  @Bean fun encoder() = PasswordEncoderFactories.createDelegatingPasswordEncoder()!!
+
+  @Bean
+  fun reactiveUserDetailsService(encoder: PasswordEncoder): MapReactiveUserDetailsService {
+
+    val users = mapOf(
+        "user1" to mapOf(
+            "username" to "maksimko",
+            "password" to "passwordinko"
+        )
+    )
+
+    return MapReactiveUserDetailsService(*users
+        .values
+        .map {
+          User.withUsername(it["username"])
+              .password(encoder.encode(it["password"]))
+              .authorities("USER")
+              .build()
+        }
+        .toTypedArray())
+  }
+}
+
+@SpringBootApplication
+class App
+
+fun main(args: Array<String>) {
+  runApplication<App>(*args)
+}
---
+++ src/test/java/daggerok/AppTests.kt
@@ -0,0 +1,69 @@
+package daggerok
+
+import com.codeborne.selenide.Condition.*
+import com.codeborne.selenide.Selenide.*
+import com.codeborne.selenide.WebDriverRunner.url
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.openqa.selenium.By
+import org.openqa.selenium.By.cssSelector
+import org.openqa.selenium.By.tagName
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(
+    webEnvironment = DEFINED_PORT
+//    webEnvironment = RANDOM_PORT
+)
+class `What can I say? Kotlin is awesome!`(/*@LocalServerPort val port: Int*/) {
+
+  val port = 8080
+
+  @Test
+  fun `test everything in single success flow`() {
+
+    // unauthorized user mast be redirected to login page,
+    // so sign in with credentials maksimko / passwordinko
+
+    open("http://127.0.0.1:$port")
+    sleep(3000)
+    println(`$`("body").text())
+    val form = `$`(".form-signin")
+    form.find("#username").value = "maksimko"
+    form.find("#password").value = "passwordinko"
+    form.find("button[type=submit]").click()
+
+    // assert default greeting message
+    open("http://127.0.0.1:$port/")
+    `$`("body").shouldHave(text("Hello, My friend!"))
+
+    // verify name from query params
+    open("http://127.0.0.1:$port/?name=selenidko")
+    `$`("span").shouldHave(exactText("Hello, Selenidko!"))
+
+    // and lastly verify greeting according to name from path
+    open("http://127.0.0.1:$port/lol")
+    `$`("body > span").shouldHave(exactText("Hello, Lol!"))
+
+    // go to /logout, click on button, verify sign out success
+
+    open("http://127.0.0.1:$port/logout")
+
+    `$`(cssSelector("form.form-signin"))
+        .should(exist)
+        .should(appears)
+        .shouldHave(text("Are you sure you want to log out?"))
+        .shouldHave(text("Log Out"))
+        .find(cssSelector("button[type='submit']"))
+        .click()
+
+    `$`(cssSelector("div.alert.alert-success"))
+        .should(exist)
+        .should(appear)
+        .shouldHave(text("You have been signed ou"))
+  }
+}
