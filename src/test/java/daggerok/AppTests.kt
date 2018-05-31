337ab3cba898a274484e967df0b28ed3b78e34b0
Maksim Kostromin <daggerok@gmail.com>
2018-05-31 05:25:38 +0300

    omg what have I done? (C) whatthecommit.com

--- src/test/java/daggerok/AppTests.kt
+++ src/test/java/daggerok/AppTests.kt
@@ -2,6 +2,7 @@ package daggerok
 
 import com.codeborne.selenide.Condition.*
 import com.codeborne.selenide.Selenide.*
+import com.codeborne.selenide.SelenideElement
 import com.codeborne.selenide.WebDriverRunner.url
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -18,41 +19,49 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class `What can I say? Kotlin is awesome!`(@LocalServerPort val port: Int) {
 
+  private fun String.slowPoke() {
+    println("""
+      |---
+      |${`$`(this).text()}
+      """.trimMargin())
+    sleep(1000)
+  }
+
   @Test
   fun `test everything in single success flow`() {
 
     // unauthorized user mast be redirected to login page,
     // so sign in with credentials maksimko / passwordinko
     open("http://127.0.0.1:$port")
-    sleep(1000)
-    println(`$`("body").text())
+    "body".slowPoke()
     val form = `$`(".form-signin")
     form.find("#username").value = "maksimko"
     form.find("#password").value = "passwordinko"
     form.find("button[type=submit]").click()
+    "body".slowPoke()
 
     // assert default greeting message
     open("http://127.0.0.1:$port/")
-    sleep(1000)
-    println(`$`("body").text())
+
+    "body".slowPoke()
     `$`("body").shouldHave(text("Hello, My friend!"))
 
     // verify name from query params
     open("http://127.0.0.1:$port/?name=selenidko")
-    sleep(1000)
-    println(`$`("body").text())
+
+    "body".slowPoke()
     `$`("span").shouldHave(exactText("Hello, Selenidko!"))
 
     // and lastly verify greeting according to name from path
     open("http://127.0.0.1:$port/lol")
-    sleep(1000)
-    println(`$`("body").text())
+
+    "body".slowPoke()
     `$`("body > span").shouldHave(exactText("Hello, Lol!"))
 
     // go to /logout, click on button, verify sign out success
     open("http://127.0.0.1:$port/logout")
-    sleep(1000)
-    println(`$`("body").text())
+
+    "body".slowPoke()
     `$`("form.form-signin")
         .should(exist)
         .should(appears)
@@ -61,6 +70,7 @@ class `What can I say? Kotlin is awesome!`(@LocalServerPort val port: Int) {
         .find("button[type='submit']")
         .click()
 
+    "body".slowPoke()
     `$`("div.alert.alert-success")
         .should(exist)
         .should(appear)
